55eaaae97c87cf3de27cf699908e60b2744efbf8
redbrickyarl-web <redbrickyarl@gmail.com>
2026-09-03 15:18:15 -0400

    Wire ffi module and aethelarch feature into Cargo.toml / lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,10 @@ pub mod edge_ai;
 pub mod primitives;
 pub mod axiom;
 pub mod crypto;
+pub mod ffi;
 
 pub use cosmic_queue::PacketFrame;
 pub use edge_ai::EdgeAIPipeline;
 pub use axiom::PredatorGate;
 pub use crypto::{BwpMsmAccumulator, BssDecoder};
+pub use ffi::{AethelarchMatrix, quantize_activation, gemv, act_bytes};
